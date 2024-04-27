08977dd83c429ef614897e1222a85a400d10d035
BravoTechArena <klordbravo@gmail.com>
2024-04-27 22:28:04 +0100

    Implemented the categories and restaurants scroll view

--- src/app/(modal)/location.tsx
+++ src/app/(modal)/location.tsx
@@ -1,18 +1,50 @@
-import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
-import React, { useState } from "react";
+import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import React, { useEffect, useState } from "react";
 import MapView from "react-native-maps";
 import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
 import Colors from "@/src/constants/Colors";
 import { useNavigation } from "expo-router";
+import * as Location from "expo-location";
 
-const Location = () => {
+const MapLocation = () => {
   const navigation = useNavigation();
   const [location, setLocation] = useState({
+    longitude: 5.42343,
     latitude: 6.483552,
-    longitude: 7.511472,
     latitudeDelta: 0.05,
     longitudeDelta: 0.05,
   });
+  const [errorMsg, setErrorMsg] = useState<string | null>(null);
+
+  const apiKey = process.env.EXPO_PUBLIC_GOOGLE_API_KEY as string;
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        setErrorMsg("Permission to access location was denied");
+        return;
+      }
+
+      let res = await Location.getCurrentPositionAsync({});
+
+      let location = {
+        longitude: res.coords.longitude,
+        latitude: res.coords.latitude,
+        latitudeDelta: 0.05,
+        longitudeDelta: 0.05,
+      };
+      setLocation(location);
+    })();
+  }, []);
+
+  let text = "Waiting..";
+  if (errorMsg) {
+    text = errorMsg;
+  } else if (location) {
+    text = JSON.stringify(location);
+  }
+
   return (
     <View style={{ flex: 1 }}>
       <GooglePlacesAutocomplete
@@ -21,9 +53,10 @@ const Location = () => {
           console.log(data, details);
         }}
         query={{
-          key: process.env.EXPO_PUBLIC_GOOGLE_API_KEY,
+          key: apiKey,
           language: "en",
         }}
+        onFail={(err) => console.log(err)}
       />
       <MapView showsUserLocation={true} style={styles.map} region={location} />
       <View style={styles.absoluteBox}>
@@ -35,7 +68,7 @@ const Location = () => {
   );
 };
 
-export default Location;
+export default MapLocation;
 
 const styles = StyleSheet.create({
   map: {
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,12 +1,36 @@
-import { View, Text } from "react-native";
+import { View, Text, ScrollView, StyleSheet } from "react-native";
 import React from "react";
+import Categories from "@/src/components/Categories";
+import { SafeAreaView } from "react-native-safe-area-context";
+import Restaurants from "@/src/components/Restaurants";
 
 const Page = () => {
   return (
-    <View>
-      <Text>Page</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View>
+        <ScrollView style={{ marginBottom: 100 }}>
+          <Categories />
+          <Text style={styles.header}>Top picks in your neighborhood</Text>
+          <Restaurants />
+          <Text style={styles.header}>Offers near you</Text>
+          <Restaurants />
+        </ScrollView>
+      </View>
+    </SafeAreaView>
   );
 };
 
+const styles = StyleSheet.create({
+  container: {
+    top: 90,
+    backgroundColor: "#fff",
+  },
+  header: {
+    fontSize: 18,
+    fontWeight: "bold",
+    marginTop: 16,
+    marginBottom: 8,
+    paddingHorizontal: 16,
+  },
+});
 export default Page;
